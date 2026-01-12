02e81bd3f7493fb16d17d8fa3f51cf13c1a8af3f
ipmcknz33 <i.p.mcknz33@gmail.com>
2026-01-11 19:59:02 -1000

    i moved assets to src and made sure my import was correct

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import LibraryLogo from '../assets/library.svg ';  
+import LibraryLogo from '../assets/library.svg ';
+import { Link } from 'react-router-dom'; 
 
 const Nav = () => {
   return (
