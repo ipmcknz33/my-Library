180358491a4dd60cbf0ee3611548e75ffb79587c
ipmcknz33 <i.p.mcknz33@gmail.com>
2026-01-13 11:08:15 -1000

    highlights complete

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,5 +1,7 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import Highlight from "./ui/Highlight";
+
 
 const Highlights = () => {
   return (
@@ -9,38 +11,22 @@ const Highlights = () => {
           <h2 className="section__title">
             Why choose <span className="purple">Library</span>
           </h2>
-
           <div className="highlight__wrapper">
-            <Highlight icon={<FontAwesomeIcon icon="bolt" />} />
-            <div className="highlight">
-              <div className="highlight__img">
-               
-              </div>
-              <h3 className="highlight__subtitle">Easy and Quick</h3>
-              <p className="highlight__para">
-                Get access to the book you purchased online instantly.
-              </p>
-            </div>
-
-            <div className="highlight">
-              <div className="highlight__img">
-                <FontAwesomeIcon icon="book-open" />
-              </div>
-              <h3 className="highlight__subtitle">10,000+ Books</h3>
-              <p className="highlight__para">
-                Library has books in all your favourite categories.
-              </p>
-            </div>
-
-            <div className="highlight">
-              <div className="highlight__img">
-                <FontAwesomeIcon icon="tags" />
-              </div>
-              <h3 className="highlight__subtitle">Affordable</h3>
-              <p className="highlight__para">
-                Get your hands on popular books for as little as $10.
-              </p>
-            </div>
+            <Highlight
+              icon={<FontAwesomeIcon icon="bolt" />}
+              title="Easy and Quick"
+              description="Get access to the book you purchased online instantly."
+            />
+            <Highlight
+              icon={<FontAwesomeIcon icon="book-open" />}
+              title="10,000+ Books"
+              description="Library has books in all your favourite categories."
+            />
+            <Highlight
+              icon={<FontAwesomeIcon icon="tags" />}
+              title="Affordable"
+              description="Get your hands on popular books for as little as $10."
+            />
           </div>
         </div>
       </div>
