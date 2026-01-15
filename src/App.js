9923fc2dcbdb35a33515bd61c791fa60517545cb
ipmcknz33 <i.p.mcknz33@gmail.com>
2026-01-14 16:42:42 -1000

    fix routes and components to elements, finished Books Routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import './App.css';
-import Nav from './components/Nav';
-import Footer from './components/Footer';
-import { BrowserRouter as Router, Route }  from 'react-router-dom';
-import Home from './pages/Home';
-import Books from './pages/Books';
-
-
-
-
-
-//import Bookinfo from './pages/Bookinfo';
+import "./App.css";
+import "./index.css";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Nav />
-        <Route path="/" exact component={Home} />
-        <Route path="/books" component={Books} />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/books" element={<Books />} />
+        </Routes>
         <Footer />
       </div>
     </Router>
