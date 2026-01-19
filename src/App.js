5682e70f1b3257b0682025cc2a9bbb421bd70cdc
ipmcknz33 <i.p.mcknz33@gmail.com>
2026-01-18 16:05:10 -1000

    dynamic filtering issue

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Books from "./pages/Books";
 import { books } from "./data";
+import BookInfo from "./pages/BookInfo";
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
         <Nav />
          <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/books" element={<Books books={books} />} />
+            <Route path="/books" render={() => <Books books={books} />} />
+            <Route path="/books/1" render={() => <BookInfo books={books} />} />
           </Routes>
         <Footer />
       </div>
--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Explore = () => {
   return (
@@ -8,8 +9,9 @@ const Explore = () => {
           <h2>
             Explore more <span className="purple">Books</span>
           </h2>
-          <a href="/books"></a>
+          <Link to="/books">
           <button className="btn">Explore Books</button>
+          </Link>
         </div>
       </div>
     </section>
