cf1766795a95ea19b1e04e08443d2b6412107bdf
ipmcknz33 <i.p.mcknz33@gmail.com>
2026-01-20 13:41:12 -1000

    finished Cart. books not loading and reacrt Router flag issue

--- src/App.js
+++ src/App.js
@@ -10,28 +10,63 @@ import { books } from "./data";
 import BookInfo from "./pages/BookInfo";
 import Cart from "./pages/Cart";
 
-
 function App() {
-  const  [cart, setCart] = useState([]);
+  const [cart, setCart] = useState([]);
 
-  function  addToCart(book) {
-  setCart([...cart, book])
-}
+  function addToCart(book) {
+    setCart([...cart, { ...book, quantity: 1 }]);
+  }
+
+  function changeQuantity(book, quantity) {
+    setCart(
+      cart.map((item) =>
+        item.id === book.id
+          ? {
+              ...item,
+              quantity: +quantity,
+            }
+          : item
+      )
+    );
+  }
+
+  function removeItem(item) {
+    setCart(cart.filter(book => book.id !== item.id))
+  }
+
+  function numberOfItems() {
+    let counter = 0;
+    cart.forEach(item => {
+      counter +=item.quantity
+    })
+    return counter;
+  }
 
   useEffect(() => {
     console.log(cart);
-  }, [cart])
+  }, [cart]);
 
   return (
     <Router>
       <div className="App">
-        <Nav />
-         <Routes>
-            <Route path="/" element={<Home />} />
-           <Route path="/books" element={<Books books={books} />}/>
-           <Route path="/books/:id"  element={<BookInfo books={books} addToCart={addToCart} cart = {cart} />} />
-           <Route path="/cart" element={<Cart books={books} />}/>
-          </Routes>
+        <Nav numberOfItems={numberOfItems()} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books books={books} />} />
+          <Route
+            path="/books/:id"
+            element={
+              <BookInfo
+                books={books}
+                addToCart={addToCart}
+                cart={cart}
+                changeQuantity={changeQuantity}
+                removeItem={removeItem}
+              />
+            }
+          />
+          <Route path="/cart" element={<Cart books={books} />} />
+        </Routes>
         <Footer />
       </div>
     </Router>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,67 +1,122 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import EmptyCart from "../assets/empty_cart.svg";
 
-const Cart = () => {
+const Cart = ({ cart = [], changeQuantity, removeItem }) => {
+  const total = () => {
+    let price = 0;
+
+    cart.forEach((item) => {
+      price += (item.salePrice || item.originalPrice) * item.quantity;
+    });
+
+    return price;
+  };
+
   return (
-    <div id="books__main">
-      <div className="books__contanier">
-        <div className="row">
-          <div className="book__selected--top">
-            <h2 className="cart__title">Cart</h2>
-          </div>
-          <div className="cart">
-            <div className="cart__header">
-              <span className="cart__book">Book</span>
-              <span className="cart__quanitity">Quanitity</span>
-              <span className="cart__total">Price</span>
+    <div id="books__body">
+      <main className="books__main">
+        <div className="books__contanier">
+          <div className="row">
+            <div className="book__selected--top">
+              <h2 className="cart__title">Cart</h2>
             </div>
-            <div className="cart__body">
-                <div className="cart__item">
+
+            <div className="cart">
+              <div className="cart__header">
+                <span className="cart__book">Book</span>
+                <span className="cart__quanitity">Quanitity</span>
+                <span className="cart__total">Price</span>
+              </div>
+
+              <div className="cart__body">
+                {cart.map((book) => (
+                  <div className="cart__item" key={book.id ?? book.title}>
                     <div className="cart__book">
-                        <img src="https://m.media-amazon.com/images/I/61mIq2iJUXL._AC_UF1000,1000_QL80_.jpg" 
-                        className="cart__book--img" alt="" />
-                        <div className="cart__book--info">
-                            <span className="cart__book--title">
-                                Cracking the Code Interview
-                            </span>
-                            <span className="cart__book-price">$10.00</span>
-                            <button className="cart__book--remove">
-                                Remove
-                            </button>
-                        </div>
+                      <img src={book.url} className="cart__book--img" alt={book.title || ""} />
+
+                      <div className="cart__book--info">
+                        <span className="cart__book--title">{book.title}</span>
+
+                        <span className="cart__book-price">
+                          ${(book.salePrice || book.originalPrice).toFixed(2)}
+                        </span>
+
+                        <button
+                          className="cart__book--remove"
+                          onClick={() => removeItem(book)}
+                          type="button"
+                        >
+                          Remove
+                        </button>
+                      </div>
                     </div>
+
                     <div className="cart__quantitiy">
-                        <input type="number" min={0} max={99} className="cart__input" />
+                      <input
+                        type="number"
+                        min={0}
+                        max={99}
+                        value={book.quantity}
+                        className="cart__input"
+                        onChange={(event) =>
+                          changeQuantity(book, Number(event.target.value))
+                        }
+                      />
                     </div>
+
                     <div className="cart__total">
-                        $10.00
+                      $
+                      {(
+                        (book.salePrice || book.originalPrice) * book.quantity
+                      ).toFixed(2)}
                     </div>
+                  </div>
+                ))}
+              </div>
+
+              {cart.length === 0 && (
+                <div className="cart__empty">
+                  <img src={EmptyCart} alt="" className="cart__empty--img" />
+                  <h2>You don't have any books in your cart!</h2>
+                  <Link to="/books">
+                    <button className="btn" type="button">
+                      Browse Books
+                    </button>
+                  </Link>
                 </div>
+              )}
             </div>
-          </div>
-          <div className="total">
-            <div className="total__item total__sub-total">
-                <span>
-                    Subtotal
-                </span>
-                <span>$9.00</span>
-            </div> <div className="total__item total__tax">
-                <span>
-                    Tax
-                </span>
-                <span>$1.00</span>
-            </div><div className="total__item total__price">
-                <span>
-                    Total 
-                </span>
-                <span>$10.00</span>
-            </div>
-            <button className="btn btn__checkout no-cursor"
-                onClick={() => alert(`Haven't got around to doing this ;(`)}>
-                Proceed to Checkout
-            </button>
+
+            {cart.length > 0 && (
+              <div className="total">
+                <div className="total__item total__sub-total">
+                  <span>Subtotal</span>
+                  <span>${(total() * 0.9).toFixed(2)}</span>
+                </div>
+
+                <div className="total__item total__tax">
+                  <span>Tax</span>
+                  <span>${(total() * 0.1).toFixed(2)}</span>
+                </div>
+
+                <div className="total__item total__price">
+                  <span>Total</span>
+                  <span>${total().toFixed(2)}</span>
+                </div>
+
+                <button
+                  className="btn btn__checkout no-cursor"
+                  onClick={() => alert(`Haven't got around to doing this ;(`)}
+                  type="button"
+                >
+                  Proceed to Checkout
+                </button>
+              </div>
+            )}
           </div>
         </div>
-      </div>
+      </main>
     </div>
   );
 };
