4742436a18cb7ef21befb7f88a02a9e2502edc2a
ipmcknz33 <i.p.mcknz33@gmail.com>
2026-01-20 08:21:35 -1000

    fixed BookInfoProps finished Cart

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
          <Routes>
             <Route path="/" element={<Home />} />
            <Route path="/books" element={<Books books={books} />}/>
-           <Route path="/books/:id"  element={<BookInfo books={books} addToCart={addToCart} />} />
+           <Route path="/books/:id"  element={<BookInfo books={books} addToCart={addToCart} cart = {cart} />} />
            <Route path="/cart" element={<Cart books={books} />}/>
           </Routes>
         <Footer />
--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -8,7 +8,7 @@ import Book from "../components/ui/Book";
 
 
 
-const BookInfo = ({ books, addToCart }) => {
+const BookInfo = ({ books, addToCart, cart }) => {
   const { id } = useParams();
   const book = books.find((book) => +book.id === +id);
   const [added, setAdded] = useState(false);
